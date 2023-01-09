84d9c4db7cfacb0e39451d5ea4d6fd9853f872c7
Felipe de Moraes Alves <felipe.alves@compasso.com.br>
2023-01-09 15:12:06 -0300

    Correcao do teste da Pauta

--- avaliacao/src/test/java/com/sicredi/avaliacao/controllers/PautaControllerTest.java
+++ avaliacao/src/test/java/com/sicredi/avaliacao/controllers/PautaControllerTest.java
@@ -2,31 +2,42 @@ package com.sicredi.avaliacao.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sicredi.avaliacao.dtos.PautaForm;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.net.URI;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PautaControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	static final ObjectMapper mapper = new ObjectMapper();
 	@Test
-	public void criaPauta() throws Exception{
-		PautaForm pauta = new PautaForm(null, null);
+	public void criaPautaVazia() throws Exception{
+		PautaForm pauta = new PautaForm("", "");
 
 		URI uri = new URI("/pautas");
 
 		mockMvc.perform(
 			MockMvcRequestBuilders.post(uri).content(asJsonString(pauta)).contentType(MediaType.APPLICATION_JSON))
-			.andExpect(MockMvcResultMatchers.status().is(HttpStatus.UNPROCESSABLE_ENTITY.value()));
+			.andExpect(MockMvcResultMatchers.status().is(HttpStatus.CONFLICT.value()));
 	}
 
 	public static String asJsonString(final Object obj) {
