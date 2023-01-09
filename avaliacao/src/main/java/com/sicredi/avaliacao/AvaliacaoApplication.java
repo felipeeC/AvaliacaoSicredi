ee045e2f0984d44d2cf426085b6848a342d1308f
Felipe de Moraes Alves <felipe.alves@compasso.com.br>
2023-01-09 13:19:31 -0300

    Adicionando Swagger a aplicacao

--- avaliacao/src/main/java/com/sicredi/avaliacao/AvaliacaoApplication.java
+++ avaliacao/src/main/java/com/sicredi/avaliacao/AvaliacaoApplication.java
@@ -8,12 +8,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EntityScan("com.sicredi.avaliacao")
 @ComponentScan("com.sicredi.avaliacao")
 @EnableJpaRepositories("com.sicredi.avaliacao")
+@EnableSwagger2
 public class AvaliacaoApplication {
 
 	public static void main(String[] args) {
