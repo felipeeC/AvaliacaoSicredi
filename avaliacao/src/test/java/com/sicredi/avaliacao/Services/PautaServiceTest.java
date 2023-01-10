e8bfecd3da2ae501e0198dabf6111a96cf5803e3
Felipe de Moraes Alves <felipe.alves@compasso.com.br>
2023-01-10 14:09:45 -0300

    criando testes para classe pauta service test

--- avaliacao/src/test/java/com/sicredi/avaliacao/Services/PautaServiceTest.java
+++ avaliacao/src/test/java/com/sicredi/avaliacao/Services/PautaServiceTest.java
@@ -1,2 +1,35 @@
-package com.sicredi.avaliacao.Services;public class PautaServiceTest {
+package com.sicredi.avaliacao.Services;
+import com.sicredi.avaliacao.models.Pauta;
+import com.sicredi.avaliacao.services.PautaService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class PautaServiceTest {
+    @Autowired
+    PautaService pautaService;
+    @Test
+    public void criarPautaTituloVazio(){
+        assertTrue(pautaService.criarPauta(new Pauta("", "teste"))==null);
+    }
+    @Test
+    public void buscarPautaPorIdInexistente(){
+        try {
+            pautaService.buscarPautaPorId(40L);
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+    public void imprimirPautasSemPautasCadastradas(){
+        List<Pauta> pautas = pautaService.imprimirPautas();
+        assertTrue(pautas == null);
+    }
 }
