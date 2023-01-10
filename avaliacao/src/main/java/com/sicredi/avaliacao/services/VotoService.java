48d4456234d2d0eca7d83d81e0e57f285fe1f3f7
Felipe de Moraes Alves <felipe.alves@compasso.com.br>
2023-01-10 01:28:03 -0300

    removendo comentarios do codigo

--- avaliacao/src/main/java/com/sicredi/avaliacao/services/VotoService.java
+++ avaliacao/src/main/java/com/sicredi/avaliacao/services/VotoService.java
@@ -89,7 +89,6 @@ public class VotoService {
         }
     }
     public static boolean validarCpf(String CPF) {
-        // considera-se erro CPF's formados por uma sequencia de numeros iguais
         if (CPF.equals("00000000000") ||
                 CPF.equals("11111111111") ||
                 CPF.equals("22222222222") || CPF.equals("33333333333") ||
@@ -100,15 +99,10 @@ public class VotoService {
             return(false);
         char dig10, dig11;
         int sm, i, r, num, peso;
-        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
         try {
-            // Calculo do 1o. Digito Verificador
             sm = 0;
             peso = 10;
             for (i=0; i<9; i++) {
-                // converte o caractere do CPF em um numero:
-                // por exemplo, transforma o caractere '0' no inteiro 0
-                // (48 eh a posicao de '0' na tabela ASCII)
                 num = (int)(CPF.charAt(i) - 48);
                 sm = sm + (num * peso);
                 peso = peso - 1;
@@ -116,9 +110,7 @@ public class VotoService {
             r = 11 - (sm % 11);
             if ((r == 10) || (r == 11))
                 dig10 = '0';
-            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
-
-            // Calculo do 2o. Digito Verificador
+            else dig10 = (char)(r + 48);
             sm = 0;
             peso = 11;
             for(i=0; i<10; i++) {
@@ -130,7 +122,6 @@ public class VotoService {
             if ((r == 10) || (r == 11))
                 dig11 = '0';
             else dig11 = (char)(r + 48);
-            // Verifica se os digitos calculados conferem com os digitos informados.
             if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
                 return(true);
             else return(false);
