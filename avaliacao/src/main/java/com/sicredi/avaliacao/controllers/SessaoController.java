c5a41b7edcbbc47b8e43d1bb88504b6be7caab56
Felipe de Moraes Alves <felipe.alves@compasso.com.br>
2023-01-10 00:53:38 -0300

    code review sessaoController

--- avaliacao/src/main/java/com/sicredi/avaliacao/controllers/SessaoController.java
+++ avaliacao/src/main/java/com/sicredi/avaliacao/controllers/SessaoController.java
@@ -7,31 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.List;
 
+import javax.validation.Valid;
+import java.util.List;
 @RestController
 @RequestMapping(value = "/sessoes")
 public class SessaoController {
     @Autowired
-    private SessaoService sessaoService;
-    @PostMapping(value = "/iniciar/{idPauta}")
-    public ResponseEntity<SessaoDto> iniciar(@RequestBody @Valid SessaoForm form, @PathVariable(name = "idPauta")Long idPauta, UriComponentsBuilder uriBuilder) {
-        Sessao sessaoNova = sessaoService.criaSessao(form, idPauta);
-        if (sessaoNova != null) {
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-            //URI uri = uriBuilder.path("/sessoes/{id}").buildAndExpand(sessaoNova.getId()).toUri();
-            //return ResponseEntity.created(uri).body(new SessaoDto(sessaoNova));
-        } else {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
-    }
+    SessaoService sessaoService;
 
+    
+    @PostMapping(value = "/iniciar/{idPauta}")
+    public ResponseEntity<SessaoDto> iniciarSessao(@RequestBody @Valid SessaoForm form, @PathVariable(name = "idPauta")Long idPauta) {
+        if (sessaoService.criarSessao(form, idPauta) != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
     @GetMapping
-    public List<SessaoDto> todasSessoes(){
-        List<Sessao> sessoes = sessaoService.imprimeSessoes();
-        return SessaoDto.converter(sessoes);
+    public List<SessaoDto> imprimirTodasSessoes(){
+        return ConversorSessao.converterParaSessaoDto((sessaoService.imprimirSessoes()));
     }
 }
